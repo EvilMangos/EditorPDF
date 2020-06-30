12e23fc7d6167faf773af3d17671841ce49e21a6
EvilMangos <59839253+EvilMangos@users.noreply.github.com>
2020-06-30 20:18:21 +0300

    fix

--- test/checkFolder.js
+++ test/checkFolder.js
@@ -1,13 +1,12 @@
 const { PDFDocument } = require("pdf-lib");
 const fs = require("fs");
-let checkFolder = () => {
-  let checkFolder = (path) => {
-    fs.stat(path, async function (err) {
-      try {
-        if (err) await fs.mkdirSync(path);
-      } catch (err) {}
-    });
-  };
+
+let checkFolder = (path) => {
+  fs.stat(path, async function (err) {
+    try {
+      if (err) await fs.mkdirSync(path);
+    } catch (err) {}
+  });
 };
 
 module.exports = { checkFolder };
