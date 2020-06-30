e5605e422f22a16431b42e96dab795bf70446044
EvilMangos <59839253+EvilMangos@users.noreply.github.com>
2020-06-30 11:32:13 +0300

    Formated

--- actions/split.js
+++ actions/split.js
@@ -1,57 +1,61 @@
-try{
-const { PDFDocument } = require('pdf-lib');
-const fs = require('fs');
+try {
+  const { PDFDocument } = require("pdf-lib");
+  const fs = require("fs");
 
-let split = (path, secondPart) => work(path, secondPart).catch(err => {
-    console.log(err);
-});
-
-async function work(path, lastPageFirstPart) {
-    path = `./pdf/${path}`;
-
-    fs.stat('./result', function(err) {
-        if (err && (err.code == 'ENOENT')) {
-            fs.mkdirSync('result');
-        }
+  let split = (path, secondPart) =>
+    work(path, secondPart).catch((err) => {
+      console.log(err);
     });
 
-    try{
-        if(!fs.existsSync(path)) 
-            throw(new Error('File does not exist'));
-        } catch (err) {
-            console.log(err.message);
-    process.exit(-1);
-        }
-        const mainPDF = await PDFDocument.load(fs.readFileSync(path));
-        const firstPart = await PDFDocument.create();
-        const secondPart = await PDFDocument.create();
-        const copiedPages = await firstPart.copyPages(mainPDF, mainPDF.getPageIndices());
-        try{
-            if(mainPDF.getPages().length <= lastPageFirstPart) 
-                throw new Error("А number is too large") 
-            if(lastPageFirstPart < 1)
-                throw new Error("A number is too small") 
-        } catch {
-            console.log(err.message);
-            process.exit(-1);
-        }
-        
+  async function work(path, lastPageFirstPart) {
+    path = `./pdf/${path}`;
 
-        for (let i = 0; i < lastPageFirstPart; i++) {
-            firstPart.addPage(copiedPages[i]);
-        
-        copiedPages2 = await secondPart.copyPages(mainPDF, mainPDF.getPageIndices());
-        for(let i = lastPageFirstPart; i < mainPDF.getPages().length; i++) {
-            secondPart.addPage(copiedPages2[i]);
-        }
+    fs.stat("./result", function (err) {
+      if (err && err.code == "ENOENT") {
+        fs.mkdirSync("result");
+      }
+    });
 
-        fs.writeFileSync('./result/firstPart.pdf', await firstPart.save());
-        fs.writeFileSync('./result/secondPart.pdf', await secondPart.save());
-        }
-};
+    try {
+      if (!fs.existsSync(path)) throw new Error("File does not exist");
+    } catch (err) {
+      console.log(err.message);
+      process.exit(-1);
+    }
+    const mainPDF = await PDFDocument.load(fs.readFileSync(path));
+    const firstPart = await PDFDocument.create();
+    const secondPart = await PDFDocument.create();
+    const copiedPages = await firstPart.copyPages(
+      mainPDF,
+      mainPDF.getPageIndices()
+    );
+    try {
+      if (mainPDF.getPages().length <= lastPageFirstPart)
+        throw new Error("А number is too large");
+      if (lastPageFirstPart < 1) throw new Error("A number is too small");
+    } catch {
+      console.log(err.message);
+      process.exit(-1);
+    }
 
-module.exports = {split};
+    for (let i = 0; i < lastPageFirstPart; i++) {
+      firstPart.addPage(copiedPages[i]);
+
+      copiedPages2 = await secondPart.copyPages(
+        mainPDF,
+        mainPDF.getPageIndices()
+      );
+      for (let i = lastPageFirstPart; i < mainPDF.getPages().length; i++) {
+        secondPart.addPage(copiedPages2[i]);
+      }
+
+      fs.writeFileSync("./result/firstPart.pdf", await firstPart.save());
+      fs.writeFileSync("./result/secondPart.pdf", await secondPart.save());
+    }
+  }
+
+  module.exports = { split };
 } catch {
-    console.log(err);
-    process.exit(-1);
+  console.log(err);
+  process.exit(-1);
 }
--- main.js
+++ main.js
@@ -1,50 +1,47 @@
 // const { PDFDocument } = require('pdf-lib');
 // const fs = require('fs');
-const {merge} = require('./actions/mergepdf');
-const {split} = require('./actions/split');
-const yargs = require('yargs'); 
-const { showHelp } = require('yargs');
-
+const { merge } = require("./actions/mergepdf");
+const { split } = require("./actions/split");
+const yargs = require("yargs");
+const { showHelp } = require("yargs");
 
 yargs.command({
-    command: 'merge',
-    describe: '--file="[name1,name2,...,nameN]" -Merge some pdf files',
-    builder: {
-      file: {
-        describe: 'PDF file',
-        demandOption: true,
-        type: "string"
-      }
+  command: "merge",
+  describe: '--file="[name1,name2,...,nameN]" -Merge some pdf files',
+  builder: {
+    file: {
+      describe: "PDF file",
+      demandOption: true,
+      type: "string",
     },
-    handler: (argv) => merge(...argv.file.split(','))
-  });
+  },
+  handler: (argv) => merge(...argv.file.split(",")),
+});
 
-    yargs.command({
-    command: 'split',
-    describe: '--file="[name]" --number=[number] -Split a pdf file',
-    builder: {
-      file: {
-        describe: 'PDF file',
-        demandOption: true,
-        type: "string"
-      },
-      number: {
-        describe: 'First page of the second part after split',
-        demandOption: true,
-        type: "number"
-      }
+yargs.command({
+  command: "split",
+  describe: '--file="[name]" --number=[number] -Split a pdf file',
+  builder: {
+    file: {
+      describe: "PDF file",
+      demandOption: true,
+      type: "string",
     },
-    handler: (argv) => split(argv.file, argv.number)
-  });
+    number: {
+      describe: "First page of the second part after split",
+      demandOption: true,
+      type: "number",
+    },
+  },
+  handler: (argv) => split(argv.file, argv.number),
+});
 
-  var argv = require('yargs')
-  .option('?', {
-      describe: 'Show help',
-      type: 'boolean'
-  })
-  .argv;
+var argv = require("yargs").option("?", {
+  describe: "Show help",
+  type: "boolean",
+}).argv;
 
-  var argv = require('yargs').argv;
-  if(argv._[0] == '?') showHelp();
+var argv = require("yargs").argv;
+if (argv._[0] == "?") showHelp();
 
-  yargs.parse();
+yargs.parse();
