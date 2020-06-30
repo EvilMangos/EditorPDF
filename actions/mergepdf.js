d1e5d9d1159b8466725813065f0f2cd8b1429d36
EvilMangos <59839253+EvilMangos@users.noreply.github.com>
2020-06-30 11:37:57 +0300

    !

--- actions/mergepdf.js
+++ actions/mergepdf.js
@@ -1,39 +1,43 @@
-try{
-const { PDFDocument } = require('pdf-lib');
-const fs = require('fs');
+try {
+  const { PDFDocument } = require("pdf-lib");
+  const fs = require("fs");
 
-let merge = (...arg) => work(...arg).catch(err =>{
-  console.log(err);
-} );
+  let merge = (...arg) =>
+    work(...arg).catch((err) => {
+      console.log(err);
+    });
 
-async function work(...arg) {
-  
-  fs.stat('./result', function(err) {
-    if (err && err.code == 'ENOENT') {
-        fs.mkdirSync('result');
+  async function work(...arg) {
+    fs.stat("./result", function (err) {
+      if (err && err.code == "ENOENT") {
+        fs.mkdirSync("result");
+      }
+    });
+    g;
+
+    const doc = await PDFDocument.create();
+
+    for (let path of arg) {
+      try {
+        if (!fs.existsSync(`./pdf/${path}`))
+          throw new Error("File does not exist");
+      } catch (err) {
+        console.log(err.message);
+        process.exit(-1);
+      }
+      const content = await PDFDocument.load(fs.readFileSync(`./pdf/${path}`));
+      const contentPages = await doc.copyPages(
+        content,
+        content.getPageIndices()
+      );
+      for (const page of contentPages) {
+        doc.addPage(page);
+      }
     }
-});
-
-  const doc = await PDFDocument.create();
-
-  for(let path of arg) {
-    try{
-      if(!fs.existsSync(`./pdf/${path}`)) 
-            throw(new Error('File does not exist'));
-    } catch(err) {
-      console.log(err.message);
-            process.exit(-1);
-    }
-    const content = await PDFDocument.load(fs.readFileSync(`./pdf/${path}`));
-    const contentPages = await doc.copyPages(content, content.getPageIndices());
-  for (const page of contentPages) {
-    doc.addPage(page);
+    fs.writeFileSync("./result/result.pdf", await doc.save());
   }
-  }
-  fs.writeFileSync('./result/result.pdf', await doc.save());
-}
 
-module.exports = {merge};
+  module.exports = { merge };
 } catch {
   console.log(err);
   process.exit(-1);
