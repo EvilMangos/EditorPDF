b9417905911944b8458389245605f2eaf71f5153
EvilMangos <59839253+EvilMangos@users.noreply.github.com>
2020-07-08 11:52:37 +0300

    Fixeв

--- actions/split/splitRange.js
+++ actions/split/splitRange.js
@@ -14,9 +14,9 @@ try {
 
   async function work(source, out, range) {
     isExist(source);
+    range = getRange(range);
     const mainPDF = await PDFDocument.load(fs.readFileSync(source));
     let pathToSave = (i) => [out, i, "PDF.pdf"].join("");
-    range = getRange(range);
     for (let i = 0; i < range.length; i++) {
       savePDF(mainPDF, pathToSave(i + 1), range[i]);
       console.log(`Saved: ${pathToSave(i + 1)}`);
