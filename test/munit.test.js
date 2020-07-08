1f2fe6e2fba52ecb78e4cff6299441d45a66b62a
EvilMangos <59839253+EvilMangos@users.noreply.github.com>
2020-07-08 11:34:35 +0300

    Fixed Unit test

--- test/munit.test.js
+++ test/munit.test.js
@@ -2,14 +2,14 @@ var assert = require("assert");
 const { getRange } = require("../utility/getRange");
 
 describe("getRange", function () {
-  it("Range = 1-3,4,6-8 should return true", function () {
+  it("Range = 1-3,4,6-8", function () {
     let expectedResult = JSON.stringify([[0, 1, 2], [3], [5, 6, 7]]);
     let range = "1-3,4,6-8";
     let result = JSON.stringify(getRange(range));
     assert.equal(result, expectedResult);
   });
 
-  it("Range = 5-3 should return false", function () {
+  it("Range = 5-3", function () {
     let expectedResult = JSON.stringify([[2, 3, 4]]);
     let range = "5-3";
     let result = JSON.stringify(getRange(range));
